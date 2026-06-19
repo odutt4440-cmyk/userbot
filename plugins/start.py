a8a40e89ff072959a26ed66c7bb6c15cf6b0aa2e
odutt4440-cmyk <odutt4440@gmail.com>
2026-06-19 19:20:23 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -329,13 +329,9 @@ async def games_menu(event):
     buttons = [
         [Button.inline("WordSeek", data="mod_wordseek"), Button.inline("WordChain", data="mod_wordchain")],
         [Button.inline("Octopus", data="mod_octopus"), Button.inline("Wordly", data="mod_wordly")],
+        [Button.inline("🕵️ WordGrid", data="mod_wordgrid")],
         [Button.inline("🔙 Back to Categories", data="modules_main")]
     ]
-
-    if event.sender_id == ADMIN_ID:
-        buttons.append([Button.inline("🕵️ WordGrid (Testing)", data="mod_wordgrid")])
-    
-    buttons.append([Button.inline("🔙 Back to Categories", data="modules_main")])
 
     try:
         await event.delete() 
